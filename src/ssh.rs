449d3b716be80cce71441fb95575ba5e2f483eb8
Bojin Li <bojinxx@gmail.com>
2024-02-14 02:13:34 +1100

    feat: local file synchronization before remote command execution

--- src/ssh.rs
+++ src/ssh.rs
@@ -1,7 +1,10 @@
+use ignore::WalkBuilder;
 use ssh2::Session;
+use std::fs;
+use std::io::Write;
 use std::io::{self, Read};
 use std::net::TcpStream;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::time::Duration;
 
 pub struct AuthKey {
@@ -53,8 +56,34 @@ pub fn exec(conf: Config) -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
+    let sftp = sess.sftp()?;
+
+    let local_dir = "./";
+    // get current timestep as file name. e.g. ~/.cserun/temp/2024-02-14-01-10-40-224/
+    let temp_dir_name = chrono::Local::now()
+        .format("%Y-%m-%d-%H-%M-%S-%3f")
+        .to_string();
+    let remote_dir = format!(".cserun/temp/{}", temp_dir_name); // ssh2's sftp use ~/ as root, no need to add ~/
+    let remote_dir_path = Path::new(&remote_dir);
+    println!("remote_dir: {}", remote_dir);
+
+    // create the remote dir
+    sftp_mkdir_recursive(&sftp, remote_dir_path)?;
+    println!("Created remote dir: {:?}", remote_dir_path);
+
+    // log the command to command.txt
+    let mut remote_command_file = sftp.create(remote_dir_path.join("command.txt").as_path())?;
+    remote_command_file.write_all(conf.command.as_bytes())?;
+    println!("Uploaded command.txt");
+
+    // setup the container dir
+    let container_path = remote_dir_path.join("container");
+    upload_dir(&sftp, Path::new(local_dir), container_path.as_path())?;
+
     let mut channel = sess.channel_session()?;
-    channel.exec(conf.command.as_str())?;
+    // before exec, try to cd to the remote dir, if failed, exit
+    let command = format!("cd {}/container && {}", remote_dir, conf.command);
+    channel.exec(&command)?;
 
     // set to unblocking mode
     sess.set_blocking(false);
@@ -98,6 +127,72 @@ pub fn exec(conf: Config) -> Result<(), Box<dyn std::error::Error>> {
 
     channel.wait_close()?;
     println!("\nExit status: {}", channel.exit_status()?);
+
+    Ok(())
+}
+
+fn sftp_mkdir_recursive(sftp: &ssh2::Sftp, path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let mut current_path = PathBuf::new();
+    for component in path.components() {
+        current_path.push(component);
+        if let Ok(metadata) = sftp.stat(current_path.as_path()) {
+            if metadata.is_dir() {
+                continue;
+            }
+            return Err(format!("{:?} is not a directory", current_path).into());
+        }
+        sftp.mkdir(current_path.as_path(), 0o755)?;
+    }
+    Ok(())
+}
+
+// upload every file and directory in the local directory to remote directory
+fn upload_dir(
+    sftp: &ssh2::Sftp,
+    local_path: &Path,
+    remote_base_path: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let walker = WalkBuilder::new(local_path)
+        .ignore(true) // https://docs.rs/ignore/latest/ignore/struct.WalkBuilder.html#method.ignore
+        .git_ignore(true) // https://docs.rs/ignore/latest/ignore/struct.WalkBuilder.html#method.git_ignore
+        .build();
+
+    for result in walker {
+        if let Ok(entry) = result {
+            let path = entry.path();
+            // Calculate the relative path
+            if let Ok(strip_path) = path.strip_prefix(local_path) {
+                let remote_path = remote_base_path.join(strip_path);
+                if path.is_dir() {
+                    // Make sure the remote directory exists
+                    match sftp.mkdir(&remote_path, 0o755) {
+                        Ok(_) => println!("Created directory: {:?}", remote_path),
+                        Err(err) => {
+                            println!("Directory creation error (might already exist): {:?}", err)
+                        }
+                    }
+                } else {
+                    upload_file(sftp, path, &remote_path)?;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn upload_file(
+    sftp: &ssh2::Sftp,
+    local_path: &Path,
+    remote_path: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = fs::File::open(local_path)?;
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents)?;
+
+    let mut remote_file = sftp.create(remote_path)?;
+    remote_file.write_all(&contents)?;
+    println!("Uploaded file: {:?}", remote_path);
 
     Ok(())
 }
